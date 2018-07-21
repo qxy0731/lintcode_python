03ddc72d7237f0ada02227a00390e1b6ef960401
qxy0731 <qxy0731@163.com>
2018-07-21 12:02:42 +1000

    a

---
+++ p1042.py
@@ -0,0 +1,11 @@
+class Solution:
+    """
+    @param matrix: the given matrix
+    @return: True if and only if the matrix is Toeplitz
+    """
+    def isToeplitzMatrix(self, matrix):
+        for i in range(len(matrix)-1):
+            for j in range(len(matrix[0])-1):
+                if matrix[i][j] != matrix[i+1][j+1]:
+                    return False
+        return True
